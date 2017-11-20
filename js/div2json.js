e93dc8e61f74a36047210d169fd01f163cf4615e
jmdrake <johnmdrake@gmail.com>
2017-11-20 09:32:08 -0600

    Updated slide component.  Added div2json.js

---
+++ js/div2json.js
@@ -0,0 +1,26 @@
+function div2json(div, model, options) {
+	if(options != undefined) {
+	    var imagepath = options["imagepath"];
+		var linkpath = options["linkpath"];
+	}
+	var object = {};
+    for(var key in model) {
+        element = div.find("#" + key);
+        if(element.get(0) != undefined)            
+            switch(element.get(0).tagName){
+                case "IMG":
+                    value = element.attr("src");
+                    break;
+                case "INPUT":
+                    value = element.val();
+                    break;
+                case "A":
+                    value = element.attr("href" );
+                    break;
+                default:
+                    value = element.val();
+            }                            
+        object[key] = value;
+    }
+    return object;
+}
