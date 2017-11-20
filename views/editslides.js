8eeb7136e10f095ec585175d056c029a441a7bc2
jmdrake <johnmdrake@gmail.com>
2017-11-20 08:02:27 -0600

    Fixes to event packages.  Changes to components.

--- views/editslides.js
+++ views/editslides.js
@@ -1,15 +1,16 @@
 var slides = [
-	{"bannerimage" : "bannerdj.png", "title" : "DJs", "text" : "You only sound as good as your equipment."},
-	{"bannerimage" : "bannerwedding.png", "title" : "Weddings", "text" : "We have everything you need for your special day."},
-	{"bannerimage" : "bannercorporatesound.png", "title" : "Corporate Meetings", "text" : "Make your best first impression."}			
+	{"bannerimage" : "bannerdj.png", "title" : "DJs", "text" : "You only sound as good as your equipment.", "link":"padj.html"},
+	{"bannerimage" : "bannerwedding.png", "title" : "Weddings", "text" : "We have everything you need for your special day.", "link":"pawedding.html"},
+	{"bannerimage" : "bannercorporatesound.png", "title" : "Corporate Meetings", "text" : "Make your best first impression.", "link":"pacorporate.html"}			
 ];
 
-$(document).ready(function(){
-   $("#slideshow").load("slideshow.html", function(){		 
-		 var slidetemplate = cloneDiv($("#tmplSlide"));
-		 $("#slidelist").html("");
-		 populateDivList($("#slidelist"), slides, slidetemplate, {"imagepath" : "../images/"});		 
-	 }); 
+$(document).ready(function () {
+	$("#slideshow").load("components/slide.html", function () {
+	    var slidetemplate = cloneDiv($("#tmplSlide"));
+	    $("#slidelist").html("");
+	    populateDivList($("#slidelist"), slides, slidetemplate, { "imagepath": "../images/", "linkpath":"" });
+	    $(".component").attr("contenteditable", true);
+	});
 });
 
 
