9bf832af9d1dc72dad4bf1deace6ed2c6eaef53e
John Drake <johnmdrake@gmail.com>
2018-01-15 06:09:20 +0000

    wedding.html

--- views/wedding.js
+++ views/wedding.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
-    $("#cardtemplate").load("./components/card.html", function(){
-        getComponents("wedding", "card", function(results){
-            populateComponents($("#cards"), results, $("cardtemplate"));            
-        });
-        
-        getCards(function(results){
-           if(!results["error"]){
-               populateDivList($("#cards"), results, $("#cardtemplate"));
-           }
-       }) 
-    });
+	$("#cardtemplate").load("./components/card.html", function(){
+		getComponents("wedding", "card", function(results){
+			populateComponents($("#cards"), results, $("cardtemplate"));            
+		});        
+	});
 });
 
