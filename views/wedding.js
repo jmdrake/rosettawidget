8852f5af42274d3fddd2c6727576e9822abda441
John Drake <johnmdrake@gmail.com>
2018-01-15 06:22:22 +0000

    fix to wedding.js

--- views/wedding.js
+++ views/wedding.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	$("#cardtemplate").load("./components/card.html", function(){
-		getComponents("wedding", "card", function(results){
+		getComponents("card", "wedding", function(results){
 			populateComponents($("#cards"), results, $("cardtemplate"));            
 		});        
 	});
