ab0761a60d94ceaf564254fb3658bfb388442e23
jmdrake <johnmdrake@gmail.com>
2018-01-27 00:14:03 +0000

    Updates

--- views/wedding.js
+++ views/wedding.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+	var url = location.href;
+	if(url.match(/\?edit/)){
+		$("#cardtemplate").load("./components/editcard.html", function(){
+			getComponents("card", "wedding", function(results){
+				populateDivList($("#cards"), results, $("#cardtemplate"), {"imagepath":"../images/weddings/"});
+			});        
+		});		
+	}
 	$("#cardtemplate").load("./components/card.html", function(){
 		getComponents("card", "wedding", function(results){
 			populateDivList($("#cards"), results, $("#cardtemplate"), {"imagepath":"../images/weddings/"});
